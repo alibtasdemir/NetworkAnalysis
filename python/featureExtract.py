7513517c2ae2f7a6be1dd87ed7c195f3dad51d06
abt <osboxes@abt.localdomain>
2019-11-13 17:59:36 +0300

    Process file added to automate feature extraction.

--- python/featureExtract.py
+++ python/featureExtract.py
@@ -3,13 +3,18 @@ import numpy as np
 import pandas as pd
 import os
 import glob
+import time
 
+start_time = time.time()
+# The graph folder.
 graph_folder = os.path.abspath("graphs/")
+# File pattern of the graph files.
 file_pattern = os.path.join(graph_folder, "*.edges")
 
+# Extracted features.
+# To create data frame.
 feature_categories = np.array(["Name", "Vertex", "Edges", "Density", "Max. Degree", "Avg. Degree", "Max. k-core", "Avg. Clustering Coeff.",
                       "Diameter", "Avg. Path Length", "Total Triangles", "Avg. Eigenvector Centrality"])
-
 
 
 def getFeatureVect(G, name="NoName"):
@@ -33,20 +38,34 @@ def getFeatureVect(G, name="NoName"):
     values.append(np.mean(np.array(list(nx.eigenvector_centrality_numpy(G).values()))))
     return np.array(values)
 
-df = pd.DataFrame(columns=feature_categories)
-i = 0
+if __name__ == "__main__":
+	# Empty data frame created
+	df = pd.DataFrame(columns=feature_categories)
+	# Index counter
+	i = 0
 
-for filename in glob.glob(file_pattern):
-    print(filename)
-    G = nx.read_edgelist(filename)
-    file = os.path.basename(filename)
-    G = nx.read_edgelist(filename)
-    data = getFeatureVect(G, file)
-    df.loc[i] = data
-    i+=1
-    del(G)
-    print("%s --- Extracted"%(file))
+	# Scanning for all files with defined file pattern. 
+	for filename in glob.glob(file_pattern):
+		print(filename)
+		file = os.path.basename(filename)
+		# Creating graph from edge list
+		read_time = time.time()
+		G = nx.read_edgelist(filename)
+		print("##### %s --- Read completed in %s seconds" %(file, time.time() - read_time))
+		# Calculating graph's features
+		feature_time = time.time()
+		data = getFeatureVect(G, file)
+		print("##### %s --- Calculations completed in %s seconds" %(file, time.time() - feature_time))
+		# Placing on data frame
+		df.loc[i] = data
+		i+=1
+		# Deleting graph to save memory
+		del(G)
+		print("%s --- Extracted"%(file))
 
-with open("features.csv", "w", newline='') as f:
-    f.write(df.to_csv())
-    f.close()
+	# Creating a csv to save features.
+	with open("features.csv", "w", newline='') as f:
+	    f.write(df.to_csv())
+	    f.close()
+	
+	print("##### Total execution time is %s seconds" % (time.time()-start_time))
